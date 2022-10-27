361f530a9e52cd5a6aa4a07393a3b019b5971bb3
EC2 Default User <ec2-user@ip-172-31-33-174.us-east-2.compute.internal>
2022-10-27 21:28:58 +0000

    vpc_v3

--- vpc_v3/gateway.tf
+++ vpc_v3/gateway.tf
@@ -1,4 +1,14 @@
 resource "aws_internet_gateway" "ingw" {
  vpc_id = aws_vpc.vpc_v3.id
- tags = merge(local.comman_tags, {Name = replace(local.name, "rtype" , "ingw")})   
+ tags = merge(local.common_tags, {Name = replace(local.name, "rtype" , "ingw")})   
 }
+
+resource "aws_eip" "eip"{
+    vpc = true
+}
+
+resource "aws_nat_gateway" "nat_gateway" {
+  subnet_id         = aws_subnet.public_subnet[0].id
+  allocation_id = aws_eip.eip.id
+  tags = merge(local.common_tags, { Name = replace(local.name, "rtype", "nat")})
+  }
--- vpc_v3/route_table.tf
+++ vpc_v3/route_table.tf
@@ -1,8 +1,33 @@
 resource "aws_route_table" "public_rt" {
     vpc_id = aws_vpc.vpc_v3.id
    route {
-    cidr_block = 0.0.0.0/0
-    gateway_id = 
+    
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ingw.id
+   }
+tags = merge(local.common_tags, { Name = replace(local.name, "rtype", "public_rt")})
+    
+}
 
-    }
+resource "aws_route_table_association" "public_subnet" {
+    count = length(var.private_subnet)
+    subnet_id = element(aws_subnet.public_subnet.*.id, count.index)
+    route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table" "private_rt" {
+    vpc_id = aws_vpc.vpc_v3.id
+   route {
+    
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
+}
+    tags = merge(local.common_tags, { Name = replace(local.name, "rtype", "private_rt")})
+    
+}
+
+resource "aws_route_table_association" "private_subnet" {
+    count = length(var.private_subnet)
+    subnet_id = element(aws_subnet.private_subnet.*.id, count.index)
+    route_table_id = aws_route_table.private_rt.id
 }
